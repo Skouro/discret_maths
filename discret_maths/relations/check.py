e8cea5b0115459b9ac832d1003fec55bb79d329f
Diego Restrepo <drestrepo@fluidattacks.com>
2021-03-26 20:10:07 -0500

    feat(back): add check module
    - module to validate type of relations

---
+++ discret_maths/relations/check.py
@@ -0,0 +1,133 @@
+# Standar import
+
+# Third imports
+from typing import Optional, Set
+from networkx import DiGraph
+
+
+def is_reflexive(
+    graph: DiGraph,
+    nodes: Optional[Set[int]] = None,
+) -> bool:
+    nodes = nodes or graph.nodes
+    return all(graph.has_edge(node, node) for node in nodes)
+
+
+def is_anti_reflexive(
+    graph: DiGraph,
+    nodes: Optional[Set[int]] = None,
+) -> bool:
+    nodes = nodes or graph.nodes
+    return all(not graph.has_edge(node, node) for node in nodes)
+
+
+def is_not_reflexive(
+    graph: DiGraph,
+    nodes: Optional[Set[int]] = None,
+) -> bool:
+    nodes = nodes or graph.nodes
+    return any(graph.has_edge(node, node) for node in nodes)
+
+
+def is_symmetric(
+    graph: DiGraph,
+    nodes: Optional[Set[int]] = None,
+) -> bool:
+    nodes = nodes or graph.nodes
+    return all(
+        graph.has_edge(y, x) for x in nodes for y in nodes
+        if x != y and graph.has_edge(x, y))
+
+
+def is_anti_symmetric(
+    graph: DiGraph,
+    nodes: Optional[Set[int]] = None,
+) -> bool:
+    nodes = nodes or graph.nodes
+    return all(not graph.has_edge(y, x) for x in nodes for y in nodes if x != y
+               if graph.has_edge(x, y))
+
+
+def is_not_symmetric(
+    graph: DiGraph,
+    nodes: Optional[Set[int]] = None,
+) -> bool:
+    nodes = nodes or graph.nodes
+
+    if is_symmetric(graph, nodes):
+        return False
+
+    return any(
+        graph.has_edge(y, x) for x in nodes for y in nodes if x != y
+        if graph.has_edge(x, y))
+
+
+def is_transitive(
+    graph: DiGraph,
+    nodes: Optional[Set[int]] = None,
+) -> bool:
+    nodes = nodes or graph.nodes
+    return all(
+        graph.has_edge(x, z) for x in nodes for y in nodes
+        if x != y and graph.has_edge(x, y) for z in graph.adj[y] if y != z)
+
+
+def is_not_transitive(
+    graph: DiGraph,
+    nodes: Optional[Set[int]] = None,
+) -> bool:
+    nodes = nodes or graph.nodes
+    return any(not graph.has_edge(x, z) for x in nodes for y in nodes
+               if x != y and graph.has_edge(x, y) for z in graph.adj[y]
+               if y != z)
+
+
+def is_equivalent(
+    graph: DiGraph,
+    nodes: Optional[Set[int]] = None,
+) -> bool:
+    nodes = nodes or graph.nodes
+    reflexive = is_reflexive(graph, nodes)
+    symmetric = is_symmetric(graph, nodes)
+    transitive = is_transitive(graph, nodes)
+
+    return reflexive and symmetric and transitive
+
+
+def is_strict_order(
+    graph: DiGraph,
+    nodes: Optional[Set[int]] = None,
+) -> bool:
+    nodes = nodes or graph.nodes
+    transitive = is_transitive(graph, nodes)
+    anti = is_anti_symmetric(graph, nodes)
+
+    return anti and transitive
+
+
+def is_partial_order(
+    graph: DiGraph,
+    nodes: Optional[Set[int]] = None,
+) -> bool:
+    nodes = nodes or graph.nodes
+    reflexive = is_reflexive(graph, nodes)
+    anti_symmetric = is_anti_symmetric(graph, nodes)
+    transitive = is_transitive(graph, nodes)
+
+    return reflexive and anti_symmetric and transitive
+
+
+def is_total_order(
+    graph: DiGraph,
+    nodes: Optional[Set[int]] = None,
+) -> bool:
+    nodes = nodes or graph.nodes
+
+    reflexive = is_reflexive(graph, nodes)
+    anti_symmetric = is_anti_symmetric(graph, nodes)
+    transitive = is_transitive(graph, nodes)
+    all_relatione = all(
+        graph.has_edge(x, y) and graph.has_edge(y, x) for x in nodes
+        for y in nodes)
+
+    return reflexive and anti_symmetric and transitive and all_relatione
